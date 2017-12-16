521c7e241022cb92b1236ca072c66548bbe56125
Ivan Sharamok <isharamok@gmail.com>
2017-12-15 21:18:41 -0800

    removed search js method

--- docs/static/js/script.js
+++ docs/static/js/script.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
     console.log("Loading document");
 
     backToTop();
-    search();
+    // search is disabled
+    //search();
 });
 
 /**
@@ -42,14 +43,15 @@ function backToTop() {
 
 }
 
-function search(){
-    (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
-        (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
-        e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.appendChild(s);
-    })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+// disabled since SwifType search isn't used.
+// function search(){
+//     (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+//         (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+//         e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.appendChild(s);
+//     })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
 
-    _st('install','{{site.swiftype_searchId}}','2.0.0');
-}
+//     _st('install','{{site.swiftype_searchId}}','2.0.0');
+// }
 
 
 
