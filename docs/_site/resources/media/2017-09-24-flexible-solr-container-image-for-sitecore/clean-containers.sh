0f1c4ac0bebb6fcb6f9cea9338a5f1928a06693c
Ivan Sharamok <isharamok@gmail.com>
2017-12-15 20:55:46 -0800

    removed _site

--- docs/_site/resources/media/2017-09-24-flexible-solr-container-image-for-sitecore/clean-containers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# remove all containers based on image
-IMAGE=${1}
-REMOREIMAGE=${2}
-docker ps -a | awk '{ print $1,$2 }' | grep $IMAGE | awk '{print $1 }' | xargs -I {} docker -f rm {}
-if [[ "rmi" == $REMOREIMAGE ]]; then
-    docker rmi $IMAGE -f
-fi
